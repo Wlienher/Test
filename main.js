ab9910c1cc71cbb2c62fc94e7722e2460d6ca08d
Wlienher <kliokenay@gmail.com>
2024-10-01 16:51:44 -0400

    "Mas pruebas"

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("%c Solade", azur)
+let azur = `
+    color: cyan;
+    background: black;
+    font-size: 60px;
+    `
+console.log("%c Sin Saldo", azur)
+
+let None = document.getElementById;
+
+// alert (None)
+
+function nombre () {
+    alert("Hola Mundo");
+}
+
+// Notación tradicional
+const mivar = function () {
+    alert("Estoy aprendiendo Javascript");
+}
+
+// Notación de flecha
+let otravar = (a, b) => {
+    alert("El resultado de la suma es " + (a + b));
+}
+
+// mivar();
+otravar(5, 15);
